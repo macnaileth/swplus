35977e1a238559d4ebbab8f84f5c660e9fe9916d
macnaileth <mac@tsu-nami.de>
2022-07-08 14:18:23 +0200

    Started Parser construction for icf json

--- src/components/SWCodeSearch.jsx
+++ src/components/SWCodeSearch.jsx
@@ -9,43 +9,72 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
 
-import StatusBadge from '../components/StatusBadge.jsx';
+//internal
+import StatusBadge from '../components/StatusBadge';
+import ManParse from '../lib/ManParse';
+
+//json manuals
+import manualsWHO from '../lib/manuals';
+Object.freeze(manualsWHO);
+
 
 //custom react classes
 export class SWCodeSearch extends React.Component {
     
     constructor(props) {
         super(props);
-        this.state = {code: '', msg: '', submit: 'disabled', icd10: false, icf: false};
+        this.state = {  code: '', 
+                        msg: '',  
+                        icd10: false, 
+                        icf: false,
+                        codetitle: ''
+                     };
         this.handleCodeInput = this.handleCodeInput.bind(this);
         this.matchICDBooks = this.matchICDBooks.bind(this);
         this.handleSubmitCode = this.handleSubmitCode.bind(this);
+        //declare icd/icf parser for use
+        this.parser = new ManParse(manualsWHO);
     }
     
     matchICDBooks(string) {
         //check regex patterns
         const regexPat = { icd: /^[A-TV-Z]{1}[0-9]{2}\.?[0-9]{0,2}[GVAZRLB]{0,2}$/gmi,
-                           icf: /^[bsde][1-9][0-9]{0,4}$/gmi };
+                           icf: /^[bsde]{1}[1-9]{0,1}[0-9]{0,4}$/gmi };
         if( string.length > 3 && !string.includes('.') ) { 
             this.setState({icd10: false}); 
         } else {
             regexPat.icd.test(string) ? this.setState({icd10: true}) : this.setState({icd10: false}); 
         } 
-        regexPat.icf.test(string) ? this.setState({icf: true}) : this.setState({icf: false}); 
+        if( string.length !== 3 ){
+            regexPat.icf.test(string) ? this.setState({icf: true}, () => {
+                string.length === 1 && this.setState({ msg: 'ICF-Kapitel' });
+                string.length === 2 && this.setState({ msg: 'ICF-Komponente' });
+                string.length > 3 && this.setState({ msg: 'ICF-Code' });
+                this.setState({ codetitle: this.parser.icfTitle(string) });    
+            }) : this.setState({icf: false, msg: '', codetitle: ''}); 
+        } else {
+            this.setState({ icf: false, msg: '', codetitle: '' });
+        }
     }
     
     handleCodeInput(event) {
         //if we have a valid code, unlock the button for submission
         this.setState({code: event.target.value}, () => {
-            event.target.value < 3 ? this.setState({msg: 'warning:Code zu kurz', submit: 'disabled'}) : this.setState({msg: '', submit: ''});
             this.matchICDBooks(event.target.value);
-            console.log("Current Code: " + this.state.code + ", Code length: " + this.state.code.length);
         });
     }
-    
-    handleSubmitCode() {
-        const queryString = '?Code=' + this.state.code + '&icd10=' + this.state.icd10 + '&icf=' + this.state.icf;
-        console.log('API Query:  ' + queryString);
+    //supports API for querying and API or LOCAL for using local data files (xml/json)
+    handleSubmitCode(process = 'API') {
+        if (process === 'API') {
+            const queryString = '?Code=' + this.state.code + '&icd10=' + this.state.icd10 + '&icf=' + this.state.icf;
+        } else if (process === 'LOCAL') {
+            //process full code information
+            if (this.state.icf) {
+                console.log('It is a ICF Code!');
+            }
+            console.log('We do it Local:  ', manualsWHO.icf);
+        }
+        
     }
     
     render() {
@@ -53,8 +82,8 @@ export class SWCodeSearch extends React.Component {
             <div>
                 <InputGroup className="my-3">
                     <Form.Control
-                      placeholder="ICD10 oder ICF Code"
-                      aria-label="ICD10 oder ICF Code"
+                      placeholder="ICD-10 oder ICF Code"
+                      aria-label="ICD-10 oder ICF Code"
                       aria-describedby="sw_icd_icf_input"
                       maxLength="7"
                       value={ this.state.code }
@@ -63,14 +92,15 @@ export class SWCodeSearch extends React.Component {
                     <Button 
                         variant="primary" 
                         id="sw_icd_icf_button" 
-                        className={ this.state.submit }
-                        onClick={ () => { this.handleSubmitCode(); } }>
+                        className={ this.state.icd10 || this.state.icf ? '' : 'disabled' }
+                        onClick={ () => { this.handleSubmitCode('LOCAL'); } }>
                       { this.props.buttonText === undefined ? 'Go!' : this.props.buttonText }
                     </Button>
                 </InputGroup> 
-                { this.state.msg && <StatusBadge BadgeData={ this.state.msg }/> }
-                { this.state.icd10 && <StatusBadge BadgeData="ICD-10(?)"/> }
-                { this.state.icf && <StatusBadge BadgeData="ICF(?)"/> }
+                { this.state.icd10 && <StatusBadge BadgeData="ICD-10"/> }
+                { this.state.icf && <StatusBadge BadgeData="ICF"/> }
+                { this.state.msg && <StatusBadge BadgeData={ "dark:" + this.state.msg }/> }
+                { this.state.codetitle && <StatusBadge BadgeData={ "info:" + this.state.codetitle }/> }
             </div>
                );
     }    
