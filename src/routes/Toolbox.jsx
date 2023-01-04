8eb808b17add67183826bbd8c9723780c9f0f0ed
marconagel <marconagel@192.168.1.23>
2023-01-04 21:19:43 +0100

    Now using react's lazy load feature to split the code

--- src/routes/Toolbox.jsx
+++ src/routes/Toolbox.jsx
@@ -1,17 +1,24 @@
 /**
  * Toolbox page
  */
-import React, { useState } from 'react';
+import React, { useState, Suspense } from 'react';
 //external ressources
 //react-bootatstrap components
 import Tabs from 'react-bootstrap/Tabs';
 import Tab from 'react-bootstrap/Tab';
 
-//internal ressources
+//internal ressources - old school
+/*
 import SWCodeSearch from '../components/SWCodeSearch.jsx';
 import SWCodeContainer from '../components/SWCodeContainer.jsx';
 import SWBPSModel from '../components/SWBPSModel.jsx';
 import SWICFDataExport from '../components/SWICFDataExport.jsx';
+*/
+//internal ressources - lazy load
+const SWCodeSearch = React.lazy(() => import('../components/SWCodeSearch.jsx'));
+const SWCodeContainer = React.lazy(() => import('../components/SWCodeContainer.jsx'));
+const SWBPSModel = React.lazy(() => import('../components/SWBPSModel.jsx'));
+const SWICFDataExport = React.lazy(() => import('../components/SWICFDataExport.jsx'));
 
 export default function Toolbox() {
   
@@ -33,35 +40,48 @@ export default function Toolbox() {
     
     console.log( 'Remove triggered for: ' + strCode + ', type: ' + strType );
   }
+  
+  const LoadingCircle = () => {
+      return ( 
+                <React.Fragment>
+                    <div className="d-flex justify-content-center">
+                        <div className="spinner-border text-dark me-2" role="status"><span className="visually-hidden">Lade Toolbox...</span></div> 
+                        <div className="text-dark pt-1"><strong>Lade Toolbox - Bitte warten...</strong></div>
+                    </div>
+                </React.Fragment>
+             );
+  }
     
   return (
-    <div className="py-5">       
-        <Tabs defaultActiveKey="codesearch" id="sw_toolbox_tabs" className="mb-3">
-            <Tab eventKey="codesearch" title="Code">
-                <h1 className="display-6 text-secondary text-center">ICD-10 - ICF Codesuche</h1>
-                <SWCodeSearch 
-                    buttonText = "Suchen!" 
-                    handler = { updateCodesHandler }
-                />            
-            </Tab>
-            <Tab eventKey="BPSModel" title="BPSM">
-                <h1 className="display-6 text-secondary text-center">Biopsychosoziales Modell der ICF</h1>
-                <SWBPSModel 
-                    selectedCodes={ codes } 
-                    className="my-4"
-                    handler = { updateCodesHandler }
-                />
-            </Tab>   
-            <Tab eventKey="listexport" title="Export">
-                <h1 className="display-6 text-secondary text-center">Daten exportieren</h1>
-                <SWICFDataExport selectedCodes={ codes } />
-            </Tab>               
-        </Tabs>
-        <SWCodeContainer 
-            className="py-2"
-            selectedCodes={ codes }
-            removeHandler={ removeCodesHandler }
-        />
+    <div className="py-5">  
+        <Suspense fallback={ <LoadingCircle /> }>
+            <Tabs defaultActiveKey="codesearch" id="sw_toolbox_tabs" className="mb-3">
+                <Tab eventKey="codesearch" title="Code">
+                    <h1 className="display-6 text-secondary text-center">ICD-10 - ICF Codesuche</h1>
+                    <SWCodeSearch 
+                        buttonText = "Suchen!" 
+                        handler = { updateCodesHandler }
+                    />            
+                </Tab>
+                <Tab eventKey="BPSModel" title="BPSM">
+                    <h1 className="display-6 text-secondary text-center">Biopsychosoziales Modell der ICF</h1>
+                    <SWBPSModel 
+                        selectedCodes={ codes } 
+                        className="my-4"
+                        handler = { updateCodesHandler }
+                    />
+                </Tab>   
+                <Tab eventKey="listexport" title="Export">
+                    <h1 className="display-6 text-secondary text-center">Daten exportieren</h1>
+                    <SWICFDataExport selectedCodes={ codes } />
+                </Tab>               
+            </Tabs>
+            <SWCodeContainer 
+                className="py-2"
+                selectedCodes={ codes }
+                removeHandler={ removeCodesHandler }
+            />
+        </Suspense>
     </div>
   );
 }
